2e39ad4dd2833272cc51f6b0b6de68bf46ed07ac
Hector Ochando <ochandoca@esat-alumni.com>
2022-10-15 21:36:05 +0200

    Modify the compiler script to delete unwanted files from the bin directory before compile

--- build/compile_cl.bat
+++ build/compile_cl.bat
@@ -3,6 +3,9 @@
 cls
 
 del *.obj *.pdb *.ilk
+del ../bin/*.pdb
+del ../bin/*.ilk
+del ../bin/*.exe
 
 cl /nologo /Zi /GR- /EHs /W4 /MDd /c -I ..\include -I ..\deps\esat\include  ..\src\*.cc
 cl /nologo /Zi /GR- /EHs /W4 /MDd /Fe:../bin/test.exe *.obj ..\deps\esat\bin\ESAT_d.lib opengl32.lib user32.lib gdi32.lib shell32.lib

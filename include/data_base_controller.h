1f351bdd7c5e1a4ba41d84b6b3620577448276d5
Hector Ochando <ochandoca@esat-alumni.com>
2022-10-15 21:51:00 +0200

    Set a correct sintax acording the ESAT normative

--- include/data_base_controller.h
+++ include/data_base_controller.h
@@ -1,69 +1,60 @@
-/*
-    * Data Base functions declarations
-    * Author Xema Maestre && HEctor Ochando
-    * <maestrequi@esat-alumni.com>
-*/
+/**
+ * @file data_base_controller.h
+ * @author Xema Maestre <maestrequi@esat-alumni.com>
+ * @author Hector Ochando <ochandoca@esat-alumni.com>
+ * @brief 
+ * 
+ */
 
 
 #ifndef __DATA_BASE_CONTROLLER_H__ 
 #define __DATA_BASE_CONTROLLER_H__ 1 
 
-#include "esat_extra/sqlite3.h"
-
-
+#include <esat_extra/sqlite3.h>
 
 class DataBaseController{
-    public:
-    // Public functions
-        
-        static DataBaseController& Instance();
-        
-        //** Importante **
-        // Constructor de copia
-        DataBaseController(const DataBaseController&) = delete; // Palabra clave, para decirle que no exista ese constructor. Tambien se puede poner default en vez de delete para que lo haga el compilador por defecto
-        void operator = (const DataBaseController&) = delete; // Sobre carga de operador para la clase
-        ~DataBaseController(); // Destructor
+ public:
+  // Public functions
+  
+  static DataBaseController& Instance();
 
-        void ShowWindow();
-        bool OpenDB(char *name);
+  //** Importante **
+  // Constructor de copia
+  DataBaseController(const DataBaseController&) = delete; // Palabra clave, para decirle que no exista ese constructor. Tambien se puede poner default en vez de delete para que lo haga el compilador por defecto
 
-        void SetNumTables(int);
-        void SetTableName(int, char*);
+  ~DataBaseController(); // Destructor
 
-        void GetTablesName();
+  void operator = (const DataBaseController&) = delete; // Sobre carga de operador para la clase
 
-    private:
-    // Private functions
-    sqlite3* db;
-    char **tables_name;
-    int num_tables;
-    char *err_msg;
+  void ShowWindow();
+  bool OpenDB(char *name);
+  void SetNumTables(int);
+  void SetTableName(int, char*);
+  void GetTablesName();
 
+ private:
+  // Private functions
+  sqlite3* db_;
+  char **tables_name_;
+  int num_tables_;
+  char *err_msg_;
 
-    
-    // Constructor
-    DataBaseController();
-    
-    void ShowTable();
-    
+  // Constructor
+  DataBaseController();
 
-    // Consulta Select
-    void ExecuteSelect(char *query); // Devolverá una tabla
-
-    // Cualquier tipo de consulta excepto Select
-    void ExecuteQuery(char *query);
-
-    // Ventana principal
-    void MainWindow();
-
-    // Zona de eleccion de tabla
-    void TablesNameWindow();
-    
-    // Mostrar contenido de tabla
-    void PreviewWindow();
-    
-    // Execute user SQL
-    void SQLWindow();
+  void ShowTable();
+  // Consulta Select
+  void ExecuteSelect(char *query); // Devolverá una tabla
+  // Cualquier tipo de consulta excepto Select
+  void ExecuteQuery(char *query);
+  // Ventana principal
+  void MainWindow();
+  // Zona de eleccion de tabla
+  void TablesNameWindow();
+  // Mostrar contenido de tabla
+  void PreviewWindow();
+  // Execute user SQL
+  void SQLWindow();
 
 
 };
--- src/data_base_controller.cc
+++ src/data_base_controller.cc
@@ -1,130 +1,116 @@
-
-
-#include "data_base_controller.h"
 #include <stdio.h>
-#include "esat_extra/imgui.h"
 #include <stdlib.h>
 #include <string.h>
+#include <esat_extra/imgui.h>
+#include "data_base_controller.h"
 
 DataBaseController::DataBaseController(){}
 DataBaseController::~DataBaseController(){}
 
 DataBaseController& DataBaseController::Instance(){
-    static DataBaseController db_controller;
-    return db_controller;
+  static DataBaseController db_controller;
+  return db_controller;
 };
 
 int GetNumTables(void *notused,int num_colums, char **data, char **colum_name){
-    DataBaseController* db_ptr = (DataBaseController*) notused;
-    printf("Total de tablas-> %s\n",data[0]);
-    db_ptr->SetNumTables(atoi(data[0]));
-    return 0;
+  DataBaseController* db_ptr = (DataBaseController*) notused;
+  printf("Total de tablas-> %s\n",data[0]);
+  db_ptr->SetNumTables(atoi(data[0]));
+  return 0;
 }
 
 int CallbackGetTablesName(void *notused,int num_colums, char **data, char **colum_name){
-    DataBaseController* db_ptr = (DataBaseController*) notused;
-    static int actual_pos = 0;
-    // Guardar el nombre de las tablas
-    for(int i=0; i < num_colums; i++){
-        db_ptr->SetTableName(actual_pos,data[i]);
-        //printf("Nombre tabla %d -> %s\n",actual_pos, data[i]);
-        actual_pos++;
-    }
-    return 0;
+  DataBaseController* db_ptr = (DataBaseController*) notused;
+  static int actual_pos = 0;
+  // Guardar el nombre de las tablas
+  for(int i=0; i < num_colums; i++){
+    db_ptr->SetTableName(actual_pos,data[i]);
+    //printf("Nombre tabla %d -> %s\n",actual_pos, data[i]);
+    actual_pos++;
+  }
+  return 0;
 }
 
 
 bool DataBaseController::OpenDB(char *name){
-    if(sqlite3_open_v2(name,&db,SQLITE_OPEN_READONLY,NULL) == SQLITE_OK){
-        sqlite3_close(db);
-        sqlite3_open(name,&db);
+  if(sqlite3_open_v2(name,&db_,SQLITE_OPEN_READONLY,NULL) == SQLITE_OK){
+    sqlite3_close(db_);
+    sqlite3_open(name,&db_);
 
-        // Get total tables
-        sqlite3_exec(db,"SELECT COUNT(name) FROM sqlite_master WHERE type = 'table'",GetNumTables,this,&err_msg);
-        tables_name = (char**) malloc(sizeof(char*) * (num_tables+1));
-        for(int i = 0; i < num_tables; i++){
-            tables_name[i] = (char*) malloc(sizeof(char) * 50);
-        }
-        
-        sqlite3_exec(db,"SELECT name FROM sqlite_master WHERE type = 'table'",CallbackGetTablesName,this,&err_msg);
-        GetTablesName();
-
-
-        return true;
+    // Get total tables
+    sqlite3_exec(db_,"SELECT COUNT(name) FROM sqlite_master WHERE type = 'table'",GetNumTables,this,&err_msg_);
+    tables_name_ = (char**) malloc(sizeof(char*) * (num_tables_));
+    for(int i = 0; i < num_tables_; i++){
+        tables_name_[i] = (char*) malloc(sizeof(char) * 50);
     }
+    
+    sqlite3_exec(db_,"SELECT name FROM sqlite_master WHERE type = 'table'",CallbackGetTablesName,this,&err_msg_);
+    GetTablesName();
 
-    return false;
+
+    return true;
+  }
+
+  return false;
 }
 
 void DataBaseController::ExecuteSelect(char *query){
-    char *errmsg;
-    sqlite3_exec(db,query,CallbackGetTablesName,nullptr,&errmsg);
-
-
+  char *errmsg;
+  sqlite3_exec(db_,query,CallbackGetTablesName,nullptr,&errmsg);
 }
 
 
 
 void DataBaseController::ShowWindow(){
-    MainWindow();
+  MainWindow();
 }
 
 void DataBaseController::MainWindow(){
-
-    ImGuiWindowFlags flags = ImGuiWindowFlags_::ImGuiWindowFlags_None;
-    flags |= ImGuiWindowFlags_::ImGuiWindowFlags_MenuBar;
-    flags |= ImGuiWindowFlags_::ImGuiWindowFlags_NoMove;
-    
-    if(!ImGui::Begin("DB Viewer", NULL,flags)){
-
-        ImGui::End();
-        return;
-    }
-
-    TablesNameWindow();
-
-
-
-
+  ImGuiWindowFlags flags = ImGuiWindowFlags_::ImGuiWindowFlags_None;
+  flags |= ImGuiWindowFlags_::ImGuiWindowFlags_MenuBar;
+  flags |= ImGuiWindowFlags_::ImGuiWindowFlags_NoMove;
+  
+  if(!ImGui::Begin("DB Viewer", NULL,flags)){
     ImGui::End();
+    return;
+  }
+  TablesNameWindow();
 
-
+  ImGui::End();
 }
 
 
 void DataBaseController::TablesNameWindow(){
-
-    ImVec2 size = {0,0};
-    size.x = ImGui::GetWindowSize().x * 1.0f/3.0f;
-    size.y = ImGui::GetWindowSize().y * 2.0f/3.0f;
-    if(size.x > 215.0f)size.x = 215.0f;
-    if(!ImGui::BeginChild("Tables",size,true)){
-        ImGui::EndChild();
-        return;
-    }
-
-    ImGui::Text("TABLAS");
-
-
-
+  ImVec2 size = {0,0};
+  size.x = ImGui::GetWindowSize().x * 1.0f/3.0f;
+  size.y = ImGui::GetWindowSize().y * 2.0f/3.0f;
+  if(size.x > 215.0f)size.x = 215.0f;
+  if(!ImGui::BeginChild("Tables",size,true)){
     ImGui::EndChild();
+    return;
+  }
+  ImGui::Text("TABLAS");
+
+
+
+  ImGui::EndChild();
 }
 
 
 // Setters
 void DataBaseController::SetNumTables(int num){
-    num_tables = num;
+  num_tables_ = num;
 }
 
 void DataBaseController::SetTableName(int pos, char *name){
-    //printf("Guardando en %d la tabla %s\n",pos,name);
-    strcpy(tables_name[pos], name);
+  //printf("Guardando en %d la tabla %s\n",pos,name);
+  strcpy(tables_name_[pos], name);
 }
 
 
 void DataBaseController::GetTablesName(){
-    printf("----- Nombre de las tablas -----\n");
-    for(int i = 0; i < num_tables; i++){
-            printf("%d-> %s\n",i,tables_name[i]);
-        }
+  printf("----- Nombre de las tablas -----\n");
+  for(int i = 0; i < num_tables_; i++){
+    printf("%d-> %s\n",i,tables_name_[i]);
+  }
 }

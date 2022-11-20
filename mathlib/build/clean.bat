f273f2627a6630f2204880500685a0754e90f28b
Hector Ochando <ochandoca@esat-alumni.com>
2022-11-20 18:37:52 +0100

    Update compile_cl.bat and clean.bat

--- mathlib/build/clean.bat
+++ mathlib/build/clean.bat
@@ -2,3 +2,6 @@
 
 del *.obj *.pdb *.ilk *.exe
 
+IF EXIST .\vs2019 rmdir /s /q .\vs2019
+IF EXIST ..\bin\PR00_Test rmdir /s /q ..\bin\PR00_Test
+IF EXIST ..\bin\PR01_StaticLibrary rmdir /s /q ..\bin\PR01_StaticLibrary
--- mathlib/build/compile_cl.bat
+++ mathlib/build/compile_cl.bat
@@ -2,6 +2,6 @@
 
 del *.obj *.pdb *.ilk *.exe
 
-cl /nologo /Zi /GR- /EHs /W3 /MDd /D_CRT_SECURE_NO_WARNINGS -I ..\math_library\include -I ..\unit_test\include /c ..\math_library\src\*.cc ..\unit_test\src\*.cc
+cl /nologo /Zi /GR- /EHs /W3 /MDd /D_CRT_SECURE_NO_WARNINGS -I ..\include -I ..\test\include /c ..\src\*.cc ..\test\*.cc
 cl /nologo /Zi /GR- /EHs /W3 /MDd /D_CRT_SECURE_NO_WARNINGS /Fe:unit_test_d.exe *.obj
 

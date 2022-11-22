cec418daa3eb118438b39e2f3a3c29ade075818e
Hector Ochando <ochandoca@esat-alumni.com>
2022-11-22 11:37:19 +0100

    Add solution for vs2022

--- mathlib/build/clean.bat
+++ mathlib/build/clean.bat
@@ -3,6 +3,7 @@
 del *.obj *.pdb *.ilk *.exe
 
 IF EXIST .\vs2019 rmdir /s /q .\vs2019
+IF EXIST .\vs2022 rmdir /s /q .\vs2022
 IF EXIST ..\bin\Test rmdir /s /q ..\bin\Test
 IF EXIST ..\bin\oxml rmdir /s /q ..\bin\oxml
 

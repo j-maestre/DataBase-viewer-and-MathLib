3ecce08efe2cf3649225b4aff009824869816e4e
Hector Ochando <ochandoca@esat-alumni.com>
2022-12-19 22:51:13 +0100

    update compiler for sdl

--- mathlib/build/compiler_cl_sdl.bat
+++ mathlib/build/compiler_cl_sdl.bat
@@ -4,7 +4,7 @@ cls
 
 del *.obj *.pdb *.ilk
 
-cl /nologo /Zi /GR- /EHs /W4 /MDd /c -I ..\include -I ..\deps\include\ ..\deps\bin\ImGui\*.cpp
-cl /nologo /Zi /GR- /EHs /W4 /MDd /c -I ..\include -I ..\deps\include\ ..\src\oxml\*.cc
-cl /nologo /Zi /GR- /EHs /W4 /MDd /c -I ..\include -I ..\deps\include\ ..\src\*.cc
-cl /nologo /Zi /GR- /EHs /W4 /MDd /Fe:test.exe *.obj /link /SUBSYSTEM:CONSOLE ..\deps\bin\SDL\x32\Debug\SDL2.lib ..\deps\bin\SDL\x32\Debug\SDL2main.lib shell32.lib opengl32.lib user32.lib
+cl /nologo /Zi /GR- /EHs /W4 /MDd /D_CRT_SECURE_NO_WARNINGS /c -I ..\include -I ..\deps\include\ ..\deps\bin\ImGui\*.cpp
+cl /nologo /Zi /GR- /EHs /W4 /MDd /D_CRT_SECURE_NO_WARNINGS /c -I ..\include -I ..\deps\include\ ..\src\oxml\*.cc
+cl /nologo /Zi /GR- /EHs /W4 /MDd /D_CRT_SECURE_NO_WARNINGS /c -I ..\include -I ..\deps\include\ ..\src\*.cc
+cl /nologo /Zi /GR- /EHs /W4 /MDd /D_CRT_SECURE_NO_WARNINGS /Fe:test.exe *.obj /link /SUBSYSTEM:CONSOLE ..\deps\bin\SDL\x32\Debug\SDL2.lib ..\deps\bin\SDL\x32\Debug\SDL2main.lib shell32.lib opengl32.lib user32.lib
